b6ece31e56c25380164417e7117dc86caf02322d
JGjmicode <jmi.std@yandex.ru>
2017-01-24 14:09:42 +0300

    Gex

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=cartridges',
-    'username' => 'cartridge',
-    'password' => 'Rfhnhbl:',
+    'dsn' => 'mysql:host=localhost;dbname=',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
 ];
